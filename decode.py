748eab50be2f52888e503393c274e69a7dc81c0a
josm0106 <joosm0106@gmail.com>
2026-04-05 22:18:24 +0900

    Cleanup-remove-temp-scripts-add-articles-json

--- decode.py
+++
@@ -1,7 +0,0 @@
-import base64, sys
-# Read base64 from file and decode to output file
-with open(sys.argv[1], 'r') as f:
-    b64 = f.read().strip()
-with open(sys.argv[2], 'wb') as f:
-    f.write(base64.b64decode(b64))
-print(f"Decoded {len(b64)} base64 chars to {sys.argv[2]}")
